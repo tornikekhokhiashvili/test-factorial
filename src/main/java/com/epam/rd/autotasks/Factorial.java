de80287a5ab5e59ebe007f531828735b40572f90
tornikekhokhiashvili <tornikexoxiashvili@gmail.com>
2024-06-03 23:42:59 +0400

    Initial Commit

--- src/main/java/com/epam/rd/autotasks/Factorial.java
+++ src/main/java/com/epam/rd/autotasks/Factorial.java
@@ -1,8 +1,29 @@
 package com.epam.rd.autotasks;
 
+import java.math.BigDecimal;
+
 public class Factorial {
     public String factorial(String n) {
-        throw new UnsupportedOperationException();
-
+        if (n == null) {
+            throw new IllegalArgumentException();
+        }
+        if (n.matches("^[0-9]+\\.[0-9]+$")){
+            throw new IllegalArgumentException();
+        }
+        if (n.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        if (n.matches("[^0-9]")){
+            throw new IllegalArgumentException();
+        }
+        if (Integer.parseInt(n) < 0) {
+            throw new IllegalArgumentException();
+        }
+        BigDecimal number = new BigDecimal(n);
+        BigDecimal result = new BigDecimal(String.valueOf(1));
+        for (int i = 1; i <= number.longValue(); i++) {
+            result = result.multiply(new BigDecimal(String.valueOf(i)));
+        }
+        return String.valueOf(result);
     }
 }
--- src/main/java/com/epam/rd/autotasks/FactorialCsvParametrizedTesting.java
+++ src/main/java/com/epam/rd/autotasks/FactorialCsvParametrizedTesting.java
@@ -1,7 +1,7 @@
 package com.epam.rd.autotasks;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FactorialCsvParametrizedTesting {
 
@@ -10,6 +10,6 @@ class FactorialCsvParametrizedTesting {
     @ParameterizedTest
     @CsvFileSource(resources = "/csvCases.csv")
     void testFactorial(String input, String output){
-
+        assertEquals(output, factorial.factorial(input));
     }
 }
